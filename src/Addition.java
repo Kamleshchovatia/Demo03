5acd9ae2ec171a8961c3640a55482fc319854ae7
Kamlesh Chovatia <kamlesh.chovatia19@gmail.com>
2022-05-15 12:09:23 +0100

    Addition

---
+++ src/Addition.java
@@ -0,0 +1,13 @@
+public class Addition {
+
+
+    static int a= 10;
+    static int b= 20;
+    public static void Add(){
+        System.out.println("Addition = "+ (a+b));
+    }
+
+    public static void main(String[] args) {
+        Add();
+    }
+}
